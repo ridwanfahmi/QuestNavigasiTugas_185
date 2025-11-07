6c162f0b558a6e7cf52a7d8384ff2b181bb6417b
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-08 00:59:36 +0700

    tambah spacer + column untuk layout tengah halaman

--- app/src/main/java/com/example/tugas5/view/Home.kt
+++ app/src/main/java/com/example/tugas5/view/Home.kt
@@ -1,6 +1,8 @@
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,6 +23,14 @@ fun HalamanAwal(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
     ){
+        Spacer(modifier = Modifier.height(60.dp))
 
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.Center
+        ){
+
+        }
     }
 }
