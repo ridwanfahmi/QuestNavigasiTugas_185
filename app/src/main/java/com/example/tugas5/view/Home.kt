83be8452d6bd03caa7367d618e5904cefd2d47f3
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-07 15:54:07 +0700

    nambahin composable HalamanAwal buat tampilan awal

---
+++ app/src/main/java/com/example/tugas5/view/Home.kt
@@ -0,0 +1,10 @@
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanAwal(
+    onMasukClick: () -> Unit
+){
+
+}
